bd1332bc56e76dcb52bf26d0cb4667f19d5b0305
RoopanJKR <roopanyt@gmail.com>
2022-11-17 16:14:18 +0530

    Initial Commit

---
+++ src/agribot_controller/scripts/modbus.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+from pyModbusTCP.client import ModbusClient
+
+c = ModbusClient(host="192.168.1.100",port=502,auto_open=True)
+
+try:
+    while(1):
+        
+        check = input("Do you want to check register value(y/n): ")
+        if(check=='y'):
+            regs = c.read_holding_registers(0,25)
+            print("reading")
+
+            if regs:
+                print(regs)
+            else:
+                print("error")
+            continue
+
+        addr = int(input("Enter the address: "))
+        val = int(input("Enter the value: "))
+
+        c.write_single_register(addr,val)
+
+        regs = c.read_holding_registers(0,25)
+        print("reading")
+
+        if regs:
+            print(regs)
+        else:
+            print("error")
+except(KeyboardInterrupt):
+    quit
+
