33a98d016f76bd6e76b46462429cc2b06bf711f2
RoopanJKR <roopanyt@gmail.com>
2022-11-20 16:05:01 +0530

    Modified scripts for teleop

--- src/agribot_controller/scripts/modbus.py
+++ src/agribot_controller/scripts/modbus.py
@@ -1,36 +1,60 @@
 #!/usr/bin/python3
 
+import rospy
+import roslib
+from geometry_msgs.msg import Twist
 
 from pyModbusTCP.client import ModbusClient
 
-c = ModbusClient(host="192.168.1.5",port=502,auto_open=True)
+plc = ModbusClient(host="192.168.1.5",port=502,auto_open=True)
 
-try:
-    while(1):
-        
-        check = input("Do you want to check register value(y/n): ")
-        if(check=='y'):
-            regs = c.read_holding_registers(0,25)
-            print("reading")
+wheel_bias = 0.36
+wheel_radius = 0.16
 
-            if regs:
-                print(regs)
+brake = 2
+left_reg = 4
+right_reg = 6
+
+class modbus:
+
+    def __init__(self) -> None:
+        rospy.init_node("Modbus_Velocity_sender")
+        print("Node Started")
+        self.left_rpm = 0
+        self.right_rpm = 0
+        self.write_flag = 1
+        rospy.Subscriber('/cmd_vel', Twist, self.vel_callback)
+
+
+    def vel_callback(self, msg):
+        self.vel_x = msg.linear.x
+        self.vel_z = msg.angular.z
+        while self.write_flag > 10000:
+            write_flag = 1
+            plc.write_single_register(0, self.write_flag)
+            check = plc.read_holding_registers(0, 1)
+            if check[0] == self.write_flag:
+                print("Flag Reset")
+                break
+        self.left_rpm = ((self.vel_x - (self.vel_z * wheel_bias / 2.0)) / wheel_radius) * 60 / (2*3.14159)
+        self.right_rpm = -((self.vel_x + (self.vel_z * wheel_bias / 2.0)) / wheel_radius) * 60 / (2*3.14159)
+        print("LEFT RPM = {}, RIGHT RPM = {}".format(self.left_rpm, self.right_rpm))
+
+        while(1):
+            self.write_data = [self.write_flag,self.left_rpm, self.right_rpm]
+            plc.write_multiple_registers(0,4,6,self.write_data)
+            check_flag = plc.read_holding_registers(0,1)
+            if check_flag:
+                print("Check FLAG")
             else:
-                print("error")
-            continue
+                print("Error Reading check flag")
+            if check_flag[0] == self.write_flag:
+                self.write_flag = self.write_flag + 1
+                break
 
-        addr = int(input("Enter the address: "))
-        val = int(input("Enter the value: "))
-
-        c.write_single_register(addr,val)
-
-        regs = c.read_holding_registers(0,25)
-        print("reading")
-
-        if regs:
-            print(regs)
-        else:
-            print("error")
-except(KeyboardInterrupt):
-    quit
-
+if __name__ == '__main__':
+    try:
+        start = modbus()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
